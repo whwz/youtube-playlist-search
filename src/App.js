9b648f4aae7ed321663a57144d9f6ae920895bfc
whwz <wachowicz@vp.pl>
2019-03-18 22:47:46 +0100

    add tokens

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { MDBBtn } from "mdbreact";
 import "./index.css";
+import { tokens } from "./tokens";
+
 let db = [];
 class App extends Component {
   constructor(props){
@@ -11,53 +13,10 @@ class App extends Component {
       vids: db
     }
   }
-  /* 
-   setTimeout( ()=>{for(let i = 0; i < this.state.playlists.length; i++){
-     fetch("https://www.googleapis.com/youtube/v3/playlistItems?key=AIzaSyDotJrfVw9QwJVu85Hs91Dh2DYDsvOiw6Q&part=snippet,contentDetails&playlistId=" + this.state.playlists[i].id + "&maxResults=50")
-      .then(res => res.json())
-      .then(json => {
-
-        for(let j = 0; j < json.items.length; j++)
-        {
-          if(json.items[j].snippet.title !== "Private video")
-          this.setState({ 
-            vids: [...this.state.vids, 
-                        {id: json.items[j].contentDetails.videoId,
-
-                        title: json.items[j].snippet.title,
-                        playlistId: json.items[j].snippet.playlistId}]
-                      });
-        }
-        let pages = Math.ceil(json.pageInfo.totalResults/50);
-        let nextToken = '';
-        if(pages > 1)
-          nextToken = json.nextPageToken;
-          for(let k = 1; k < pages; k++)
-          {
-            fetch("https://www.googleapis.com/youtube/v3/playlistItems?key=AIzaSyDotJrfVw9QwJVu85Hs91Dh2DYDsvOiw6Q&part=snippet,contentDetails&playlistId=" + this.state.playlists[i].id + "&maxResults=50" + "&nextPageToken=" + nextToken)
-              .then(res => res.json())
-              .then(json => {
-                for(let l = 0; l < json.items.length; l++)
-                  {
-                    if(json.items[l].snippet.title !== "Private video")
-                    this.setState({ 
-                      vids: [...this.state.vids, 
-                                  {id: json.items[l].contentDetails.videoId,
-                                  title: json.items[l].snippet.title,
-                                  playlistId: json.items[l].snippet.playlistId}]
-                                });
-                  }
-              })
-          }
-      })
-   }
-    }, 2000); */
+ 
     submitForm = (e) => {
       e.preventDefault();
-      /* this.setState({
-        currentUser: this.input.value
-      });
-      console.log(this.input.value); */
+
       this.setState({playlists: []});
       fetch("https://www.googleapis.com/youtube/v3/playlists?key=AIzaSyDotJrfVw9QwJVu85Hs91Dh2DYDsvOiw6Q&channelId=" + this.input.value + "&part=snippet,contentDetails&maxResults=50")
       .then(res => res.json())
@@ -74,14 +33,7 @@ class App extends Component {
       });
       this.input.value = '';
     }
-    /* findWithAttr = (array, attr, value) => {
-      for(let i = 0; i < array.length; i++) {
-          if(array[i][attr] === value) {
-              return i;
-          }
-      }
-      return -1;
-    } */
+
     handleClick = (e) => {
       this.setState({vids: []});
       let a = null;
@@ -94,169 +46,62 @@ class App extends Component {
     this.setState({
       playlists: arr
     })
-/* 
-    const a = findWithAttr(this.state.playlists, "id", "PLhI_w4Zkc_omQ5YfMmWOl4ZTfEkwSgtbD"); */
-      /* console.log(e.target.id);
-      console.log(a);
-      console.log(this.state); */
-    }
-  //delete private vids
- /*  handlePlaylistClick = (e) => {
-    console.log("playlist click");
-    this.setState({vids: []});
-    console.log(e.target.name);
-    const url = "https://www.googleapis.com/youtube/v3/playlistItems?key=AIzaSyDotJrfVw9QwJVu85Hs91Dh2DYDsvOiw6Q&part=snippet,contentDetails&playlistId=" + e.target.name + "&maxResults=50";
-    fetch(url)
-    .then(res => res.json())
-    .then(json => {
-      for(let i = 0; i < json.items.length; i++)
-        {
-          if(json.items[i].snippet.title !== "Private video")
-          this.setState({ 
-            vids: [...this.state.vids, 
-                        {id: json.items[i].contentDetails.videoId,
-                        title: json.items[i].snippet.title,
-                        playlistId: json.items[i].snippet.playlistId}]
-                      });
-        }
 
-      let pages = Math.ceil(json.pageInfo.totalResults/50);
-      
-      
-      if(pages > 1)
-      {
-        let nextToken = '';
-        nextToken = json.nextPageToken;
-        for(let j = 1; j < pages; j++)
-        {
-          
-          
-          console.log("j = " + j);
-          console.log(url + "&pageToken=" + nextToken);
-          fetch(url + "&pageToken=" + nextToken)
-            .then(res => res.json())
-            .then(json => {
-              for(let k = 0; k < json.items.length; k++)
-              {
-                if(json.items[k].snippet.title !== "Private video")
-                this.setState({ 
-                  vids: [...this.state.vids, 
-                              {id: json.items[k].contentDetails.videoId,
-                              title: json.items[k].snippet.title,
-                              playlistId: json.items[k].snippet.playlistId}]
-                            });
-              }
-              console.log(json.nextPageToken);
-              nextToken = json.nextPageToken;
-              console.log("here" + nextToken);
-            })
-        }
-    }
-    });
-  } */
-
-   
-
+  }
   handlePlaylistClick = (e) => {
 
-    const sleep = (milliseconds) => {
-      var start = new Date().getTime();
-      for (var i = 0; i < 1e7; i++) {
-        if ((new Date().getTime() - start) > milliseconds){
-          break;
-        }
-      }
-    }
-
-    /* console.log("playlist click"); */
-    //this.setState({vids: []});
-    /* console.log(e.target.name); */
+    db = [];
+    let pages = 0;
     let url = "https://www.googleapis.com/youtube/v3/playlistItems?key=AIzaSyDotJrfVw9QwJVu85Hs91Dh2DYDsvOiw6Q&part=snippet,contentDetails&playlistId=" + e.target.name + "&maxResults=50";
     fetch(url)
     .then(res => res.json())
     .then(json => {
-      let pages = Math.ceil(json.pageInfo.totalResults/50);
-
-      if(pages > 1)
+      //1st page
+      for(let j = 0; j < json.items.length; j++)
       {
-        console.log("pages > 1");
-        let nextToken = '';
-        nextToken = json.nextPageToken;
-        
-        
-        for(let k = 0; k < json.items.length; k++)
-        {
-          if(json.items[k].snippet.title !== "Private video")
-          db.push( 
-                    {id: json.items[k].contentDetails.videoId,
-                    title: json.items[k].snippet.title,
-                    playlistId: json.items[k].snippet.playlistId}
-                  );
-        }
-
-
-        let newUrl = url + "&pageToken=" + nextToken;
-        for(let j = 1; j < pages; j++)
-        {
-          (function(j){
-            setTimeout(function(){
-
-              console.log("j = " + j);
-          console.log("newUrl = " + newUrl);
-      
-          fetch(newUrl)
-            .then(res2 => res2.json2())
-            .then(json2 => {
-              console.log(json2);
-              for(let k = 0; k < json2.items.length; k++)
-              {
-                if(json2.items[k].snippet.title !== "Private video")
-                db.push( 
-                  {id: json2.items[k].contentDetails.videoId,
-                  title: json2.items[k].snippet.title,
-                  playlistId: json2.items[k].snippet.playlistId}
-                );
-              }
-              
-              /* console.log(json.nextPageToken);
-              nextToken = json.nextPageToken;
-              console.log("here" + nextToken); */
-              
-              nextToken = json2.nextPageToken;
-              console.log("pageToken = " + nextToken);
-              newUrl = url + "&pageToken=" + nextToken;;
-            })
-
-            }, j * 2000);
-          }(j));
-
-          
-          
-        }
-    }
-
-
-    else {
-      console.log("pages <= 1");
-      db = [];
-      for(let k = 0; k < json.items.length; k++)
-      {
-        if(json.items[k].snippet.title !== "Private video")
-          db.push( 
-                    {id: json.items[k].contentDetails.videoId,
-                    title: json.items[k].snippet.title,
-                    playlistId: json.items[k].snippet.playlistId}
+        if(json.items[j].snippet.title !== "Private video")
+                  db.push( 
+                    {id: json.items[j].contentDetails.videoId,
+                    title: json.items[j].snippet.title,
+                    playlistId: json.items[j].snippet.playlistId}
                   );
       }
-    }
-    
+                
+      pages = Math.ceil(json.pageInfo.totalResults/json.pageInfo.resultsPerPage);
+      console.log(pages);
+      let newUrl = '';
+      //1+ pages
+      if(pages > 1)
+      for(let i = 1; i < pages; i++)
+      {
+        newUrl = url + "&pageToken=" + tokens[i - 1];
+        console.log(newUrl);
+        fetch(newUrl)
+        .then(nextPageRes => nextPageRes.json())
+        .then(nextPageJson => {
 
+          for(let j = 0; j < nextPageJson.items.length; j++)
+              {
+                if(nextPageJson.items[j].snippet.title !== "Private video")
+                db.push( 
+                  {id: nextPageJson.items[j].contentDetails.videoId,
+                  title: nextPageJson.items[j].snippet.title,
+                  playlistId: nextPageJson.items[j].snippet.playlistId}
+                );
+              }
+
+              setTimeout(()=>this.setState({
+                vids: db
+              }), 200); 
+        });
+
+      } else {
+        this.setState({
+          vids: db
+        });
+      }
 
     });
-
-    setTimeout(()=>this.setState({
-      vids: db
-    }), 2000);
   }
 
 
