a8c9373e87191d2c26d7cee7ba8d7e01e17960fe
whwz <wachowicz@vp.pl>
2019-03-28 20:52:40 +0100

    add mdb elements

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
-import { MDBBtn } from "mdbreact";
+import { MDBListGroup, MDBListGroupItem, MDBBtn, MDBContainer, MDBBadge, MDBInput, MDBCol, MDBRow,  MDBFormInline, MDBIcon } from "mdbreact";
 import "./index.css";
 import { tokens, API_KEY } from "./env";
+import './App.css';
 
 let db = [];
 class App extends Component {
@@ -10,7 +11,8 @@ class App extends Component {
     this.state = {
       currentUser: '',
       playlists: [],
-      vids: db
+      vids: db,
+      filteredVids: []
     }
   } 
   /* 
@@ -60,12 +62,15 @@ class App extends Component {
                         itemCount: json.items[i].contentDetails.itemCount,
                         playlistClicked: false}]
                       });
-      }), 2000);
+      }), 500);
       this.inputName.value = '';
     }
 
     handleClick = (e) => {
-      this.setState({vids: []});
+      this.setState({
+        vids: [],
+        filteredVids: []
+      });
       const arr = [...this.state.playlists]; //store playlists state into array
       for(let i = 0; i < this.state.playlists.length; i++) 
       {
@@ -83,7 +88,6 @@ class App extends Component {
   }
 
   handlePlaylistClick = (e) => {
-
     db = [];
     let pages = 0;
     let url = "https://www.googleapis.com/youtube/v3/playlistItems?key=" + API_KEY + "&part=snippet,contentDetails&playlistId=" + e.target.name + "&maxResults=50";
@@ -126,7 +130,7 @@ class App extends Component {
 
               setTimeout(()=>this.setState({
                 vids: db
-              }), 200); 
+              }), 500);
         });
 
       } else {
@@ -138,34 +142,59 @@ class App extends Component {
     });
   }
 
-
-  
-  handlePlaylistChange = () => {
-    console.log("playlist change");
+  filterVids = (e) => {
+    const text = e.currentTarget.value;
+    const filteredVids = this.getFilteredVids(text);
+    this.setState({
+      filteredVids
+    });
   }
+
+  getFilteredVids(text) {
+    return this.state.vids.filter( vid => 
+      vid.title.toLowerCase().includes(text.toLowerCase()))
+  }
+
   render() {
     /* {console.log(this.state)} */
 
     return (
       <>
+      <MDBContainer style={{ width: "22rem" }} className="forms">{/* <MDBRow> */}
+        {/* <MDBCol md="6"> */}
         <form>
-          <input type="text" placeholder="Channel's ID" ref={input => this.inputId = input} />
-          <button type="submit" onClick={this.submitFormId}>Go</button>
+          <input className="form-control channel" type="text" placeholder="Channel's ID" ref={input => this.inputId = input} />
+          <MDBBtn className="button" color="primary" type="submit" onClick={this.submitFormId}>Go</MDBBtn>
         </form>
         <form>
-          <input type="text" placeholder="Channel's name" ref={input => this.inputName = input} />
-          <button type="submit" onClick={this.submitFormName}>Go</button>
+          <input className="form-control channel" type="text" placeholder="Channel's name" ref={input => this.inputName = input} />
+          <MDBBtn className="button" color="primary" type="submit" onClick={this.submitFormName}>Go</MDBBtn>
         </form>
+        {/* </MDBCol> */}
+      {/* </MDBRow> */}
             {/* 
             <Playlists url={this.state.currentUser} /> */}
-        {this.state.playlists.map((item, i) => item.playlistClicked ? 
-        <div key={"div" + item.id}>
-          <div id={item.id} key={i} onClick={this.handleClick}>{item.title}</div>
-          <input type="search" key={"s" + item.id} name={item.id} onClick={this.handlePlaylistClick} onInput={this.handlePlaylistChange} placeholder={"Search " + item.title + " playlist"} />
-          {this.state.vids.length ? this.state.vids./* slice(0, 10). */map((el, index) => <><li key={index}>{el.title}</li></>) : null}
-        </div>
-        : 
-        <div id={item.id} key={item.id} onClick={this.handleClick}>{item.title}</div>)}
+      </MDBContainer>
+      <MDBContainer style={{ width: "22rem" }}>
+        <MDBListGroup>
+          {this.state.playlists.map((item, i) => item.playlistClicked ? 
+          <div key={"div" + item.id}>
+            <MDBListGroupItem className="d-flex justify-content-between align-items-center"  color="warning" id={item.id} key={i} onClick={this.handleClick} active>{item.title}<MDBBadge color="primary"
+        pill>{item.itemCount}</MDBBadge></MDBListGroupItem>
+            <MDBCol>
+              <MDBFormInline className="md-form">
+                <MDBIcon icon="search" />
+                <input className="form-control form-control-sm ml-3 w-75" type="search" key={"s" + item.id} name={item.id} onClick={this.handlePlaylistClick} onInput={this.filterVids} placeholder={"Search " + item.title + " playlist"} aria-label="Search" />
+              </MDBFormInline>
+            </MDBCol>
+{/*             <input type="search" key={"s" + item.id} name={item.id} onClick={this.handlePlaylistClick} onInput={this.handlePlaylistChange} placeholder={"Search " + item.title + " playlist"} />
+ */}            {this.state.vids.length ? this.state.filteredVids./* slice(0, 10). */map((el, index) => <div class="d-flex justify-content-center"><MDBListGroupItem key={index} style={{ width: "22rem" }}><a href={"https://www.youtube.com/watch?v=" + el.id + "&list=" + item.id}>{el.title}</a></MDBListGroupItem></div>) : null}
+          </div>
+          : 
+          <MDBListGroupItem className="d-flex justify-content-between align-items-center"  color="warning" id={item.id} key={item.id} onClick={this.handleClick} hover>{item.title}<MDBBadge color="primary"
+          pill>{item.itemCount}</MDBBadge></MDBListGroupItem>)}
+        </MDBListGroup>
+        </MDBContainer>
       </>
     );
   }
